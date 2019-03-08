584a092d6fa472e26790b6b2ae477beea4773ace
carson91 <cxu51@my.bcit.ca>
2019-03-08 01:37:52 -0800

    messy search fix later

--- mobile_app/components/CatList.js
+++ mobile_app/components/CatList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, StyleSheet, TouchableHighlight, ScrollView, Dimensions, FlatList } from 'react-native';
+import { Text, View, Keyboard, TouchableWithoutFeedback, StyleSheet, TouchableHighlight, ScrollView, Dimensions, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import { LinearGradient, Font } from 'expo';
 import CatCard from './CatCard.js';
 
@@ -9,7 +10,8 @@ export default class CatList extends React.Component {
 		super(props);
 		this.state={
 			fontLoaded:false,
-			data: undefined
+			data: undefined,
+			value: null
 		}
 	}
 
@@ -22,6 +24,10 @@ export default class CatList extends React.Component {
 		})
 	}
 
+	getSearch(){
+		this.props.navigation.navigate('TempList', {cat: this.state.value})
+	}
+
 	async componentDidMount() {
 	    await Font.loadAsync({
 	      'work-sans-bold': require('../assets/WorkSans/WorkSans-Bold.ttf'),
@@ -32,21 +38,38 @@ export default class CatList extends React.Component {
 	render(){
 		let textSize = 0.04*SCREEN_HEIGHT
 		return(
-			<LinearGradient colors={['#EEEEEE','#d7d7d7']} start={[0, 0.16]} end={[0, 0.85]} style={styles.container}>
-				{this.state.fontLoaded ? (<Text style={[styles.catText, {fontSize: textSize}]}>RESOURCES</Text>) : null}
-				<FlatList
-					style={{paddingLeft: '10%', width: '100%'}}
-					contentContainerStyle={{alignItems: 'flex-start'}}
-					data = {this.state.data}
-					renderItem={({item}) => {
-						return (
-							<CatCard cat={item.type} img={item.imageFile} />
-						)}}
-					keyExtractor={item => item.type}
-					numColumns={2}
-					ItemSeparatorComponent={separator}
-			/>
-			</LinearGradient>
+			<TouchableWithoutFeedback onPress={ ()=>Keyboard.dismiss() }>
+				<LinearGradient colors={['#EEEEEE','#d7d7d7']} start={[0, 0.16]} end={[0, 0.85]} style={styles.container}>
+					{this.state.fontLoaded ? (<Text style={[styles.catText, {fontSize: textSize}]}>SEARCH</Text>) : null}
+					<SearchBar 
+					lightTheme
+					placeholder='Search for a resource'
+					placeholderTextColor='#eee'
+					searchIcon={false}
+					cancelIcon={false}
+					clearIcon={false}
+					containerStyle={styles.search}
+					inputContainerStyle={styles.searchInput}
+					inputStyle={styles.textIn}
+					onChangeText={(value)=>this.setState({value})}
+					value={this.state.value}
+					onSubmitEditing={()=>{
+						this.getSearch()
+					}}/>
+					<FlatList
+						style={{paddingLeft: '10%', width: '100%'}}
+						contentContainerStyle={{alignItems: 'flex-start'}}
+						data = {this.state.data}
+						renderItem={({item}) => {
+							return (
+								<CatCard cat={item.type} img={item.imageFile} />
+							)}}
+						keyExtractor={item => item.type}
+						numColumns={2}
+						ItemSeparatorComponent={separator}
+				/>
+				</LinearGradient>
+			</TouchableWithoutFeedback>
 	)
 }}
 
@@ -63,6 +86,21 @@ const styles = StyleSheet.create({
 	container: {
 		width:'100%',
 	    alignItems: 'center'
+	},
+	search: {
+		width: '80%',
+		borderTopWidth: 0,
+		borderBottomWidth: 0,
+		marginBottom: '2%',
+		backgroundColor: 'transparent',
+	},
+	searchInput: {
+		backgroundColor: '#aaa',
+		paddingLeft: 0,
+		paddingRight: 0,
+	},
+	textIn: {
+		color: '#4B306A'
 	},
 	catText: {
 		paddingTop:'5%',
