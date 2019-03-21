cb6e3eb3daa02e94aa5516e03fca4205eaad93bf
MaverickZephyrus <lazytagam.1@gmail.com>
2019-03-21 13:38:50 -0700

    implemented js on admin page

--- express_api/assets/js/index.js
+++ express_api/assets/js/index.js
@@ -1,3 +1,47 @@
 // js scripts for management page
 
+var summary_page = document.getElementById('summary');
+var add_page = document.getElementById('add');
+var sum_shown = true;
+var add_shown = false;
 
+
+summary_page.onclick = function() {
+    if (sum_shown == false) {
+        document.getElementById('infopanel').style.display = 'block';
+        document.getElementById('addpanel').style.display = 'none';
+
+        document.getElementById('summary').style['font-size'] = '1.8vw';
+        document.getElementById('summary').style.background = '-webkit-linear-gradient(#ee2b52, #fcb22b)';
+        document.getElementById('summary').style['-webkit-background-clip'] = 'text';
+        document.getElementById('summary').style['-webkit-text-fill-color'] = 'transparent';
+
+        document.getElementById('add').style['font-size'] = '1.5vw';
+        document.getElementById('add').style.background = 'white';
+        document.getElementById('add').style['-webkit-background-clip'] = 'text';
+        document.getElementById('add').style['-webkit-text-fill-color'] = 'transparent';
+
+        sum_shown = true;
+        add_shown = false;
+    }
+};
+
+add_page.onclick = function() {
+    if (add_shown == false) {
+        document.getElementById('infopanel').style.display = 'none';
+        document.getElementById('addpanel').style.display = 'block';
+        
+        document.getElementById('summary').style['font-size'] = '1.5vw';
+        document.getElementById('summary').style.background = 'white';
+        document.getElementById('summary').style['-webkit-background-clip'] = 'text';
+        document.getElementById('summary').style['-webkit-text-fill-color'] = 'transparent';
+        
+        document.getElementById('add').style['font-size'] = '1.8vw';
+        document.getElementById('add').style.background = '-webkit-linear-gradient(#ee2b52, #fcb22b)';
+        document.getElementById('add').style['-webkit-background-clip'] = 'text';
+        document.getElementById('add').style['-webkit-text-fill-color'] = 'transparent';
+
+        sum_shown = false;
+        add_shown = true;
+    }
+};
